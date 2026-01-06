afa0c11801f0060b7aebc9fc4e1573b6a5d72008
Andres M <marulandaandres19@gmail.com>
2026-01-06 03:48:40 -0500

    feat: lazy load page components and wrap Routes in Suspense for improved performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -7,11 +8,11 @@ import Navigation from './components/Navigation';
 import Aside from './components/Aside';
 import Profile from './components/Profile';
 
-import Home from './pages/Home';
-import Compose from './pages/Compose';
-import SignIn from './pages/Signin';
-import SignUp from './pages/SignUp';
-import Post from './pages/Post';
+const Home = lazy(() => import('./pages/Home'));
+const Compose = lazy(() => import('./pages/Compose'));
+const SignIn = lazy(() => import('./pages/Signin'));
+const SignUp = lazy(() => import('./pages/SignUp'));
+const Post = lazy(() => import('./pages/Post'));
 
 export default function App() {
   const user = null;
@@ -23,13 +24,15 @@ export default function App() {
           <div className="my-auto">{user && <Profile />}</div>
         </Col>
         <Col md={6} className="px-0 border-start border-end">
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/compose" element={<Compose />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/tweet/:id" element={<Post />} />
-          </Routes>
+          <Suspense fallback={null}>
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/compose" element={<Compose />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/tweet/:id" element={<Post />} />
+            </Routes>
+          </Suspense>
         </Col>
         <Col>
           <Aside />
