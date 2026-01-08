f9910af1a43f38d7076d62091ebc7a6c898cbe18
Andres M <marulandaandres19@gmail.com>
2026-01-08 03:14:33 -0500

    feat: add user state management with localStorage integration in UserContext

--- src/containers/UserContext.tsx
+++ src/containers/UserContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import type { Dispatch, SetStateAction } from 'react';
 
 type UserProps = {
@@ -23,6 +23,27 @@ export function UserProvider({
   children: React.ReactNode;
 }): React.ReactElement {
   const [user, setUser] = useState<UserProps>(null);
+
+  useEffect(() => {
+    const json = localStorage.getItem('user');
+    if (json) {
+      try {
+        const data = JSON.parse(json);
+        setUser(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (user === null) {
+      localStorage.removeItem('user');
+    }
+    if (user !== undefined) {
+      localStorage.setItem('user', JSON.stringify(user));
+    }
+  }, [user]);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
